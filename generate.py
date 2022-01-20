886a66e95c7d0e39119d799c70b91d9939fda72f
jdonovanCS <jordan.t.donovancs@gmail.com>
2022-01-20 12:45:56 -0500

    completed assignment through joints

--- generate.py
+++ generate.py
@@ -1,5 +1,41 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("box.sdf")
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5], size=[1,1,1])
-pyrosim.End()
+length = 1
+width = 1
+height = 1
+
+x = 0
+y = 0
+z = height+height/2
+
+
+
+def createWorld():
+    pyrosim.Start_SDF("world.sdf")
+    pyrosim.Send_Cube(name="Box", pos=[x-5,y,z], size=[length,width,height])
+    pyrosim.End()
+
+def createRobot():
+    pyrosim.Start_URDF("body.urdf")
+    pyrosim.Send_Cube(name="Torso", pos=[0,0,1.5], size=[length,width,height])
+    pyrosim.Send_Joint(name="Torso_BackLeg", parent="Torso", child="BackLeg", type="revolute", position=[-.5, 0, 1])
+    pyrosim.Send_Cube(name="BackLeg", pos=[-.5, 0, -.5], size=[length, width, height])
+    pyrosim.Send_Joint(name="Torso_FrontLeg" , parent= "Torso" , child = "FrontLeg" , type = "revolute", position = [.5,0,1])
+    pyrosim.Send_Cube(name="FrontLeg", pos=[.5, 0, -.5], size=[length, width, height])
+    pyrosim.End()
+
+createWorld()  
+createRobot()
+# pyrosim.Send_Cube(name="Box2", pos=[x+length,y,z+height], size=[length,width,height])
+
+# num_blocks=10
+# world_width = 6
+# world_length = 6
+# for k in range(world_width):
+#     y = width*k
+#     for j in range(world_length):
+#         x = length*j
+#         for i in range(num_blocks):
+#             pyrosim.Send_Cube(name="Box{}".format(i), pos=[x,y,z+(height*i)], size=[length*(.9**i),width*(.9**i),height*(.9**i)])
+
+
--- simulate.py
+++ simulate.py
@@ -1,11 +1,15 @@
 import pybullet as p
 import time
-
+import pybullet_data
 
 steps = 1000
 
 physicsClient = p.connect(p.GUI)
-p.loadSDF("box.sdf")
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+p.setGravity(0,0,-9.8)
+planeId = p.loadURDF("plane.urdf")
+robotId = p.loadURDF("body.urdf")
+p.loadSDF("world.sdf")
 for i in range(steps):
     p.stepSimulation()
     time.sleep(1/60)
