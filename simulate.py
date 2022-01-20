8a802115e67792a05b82aec2bc7e51c047615dfd
jdonovanCS <jordan.t.donovancs@gmail.com>
2022-01-20 10:47:26 -0500

    adding box to simulation

--- simulate.py
+++ simulate.py
@@ -5,10 +5,10 @@ import time
 steps = 1000
 
 physicsClient = p.connect(p.GUI)
-
+p.loadSDF("box.sdf")
 for i in range(steps):
     p.stepSimulation()
     time.sleep(1/60)
     print(i)
-    
+
 p.disconnect()
