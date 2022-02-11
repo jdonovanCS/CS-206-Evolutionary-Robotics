703b476e74ffcffb597862e319830179009c5f81
DESKTOP-ESA8F4M\Jordan <jordan.t.donovancs@gmail.com>
2022-02-11 09:40:34 -0500

    updated pyrosim to allow preparing joints correctly

--- simulate.py
+++ simulate.py
@@ -19,6 +19,7 @@ for i in range(steps):
     p.stepSimulation()
     backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
     frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
+    pyrosim.Set_Motor_For_Joint(bodyIndex=robotId, jointName="Torso_BackLeg", controlMode=p.POSITION_CONTROL, targetPosition=0.0, maxForce=500)
     time.sleep(1/60)
 np.save("data/backLegTouchSensor.npy", backLegSensorValues)
 np.save("data/frontLegTouchSensor.npy", frontLegSensorValues)
