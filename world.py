5e2f9c97179bc948d4f2dae17019e76edd243904
jdonovanCS <jordan.t.donovancs@gmail.com>
2022-04-05 11:16:08 -0400

    final project baseline

--- world.py
+++ world.py
@@ -1,6 +1,8 @@
 import pybullet as p
+import os
 
 class WORLD:
-    def __init__(self):
+    def __init__(self, solutionID):
         self.planeId = p.loadURDF("plane.urdf")
-        p.loadSDF("world.sdf")
+        p.loadSDF("world{}.sdf".format(solutionID))
+        os.system('del \"world{}.sdf\"'.format(solutionID))
