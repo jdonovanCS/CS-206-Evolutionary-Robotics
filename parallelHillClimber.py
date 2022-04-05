ec1cc5ee1832d086c049c64340e9e3e10b8280b0
jdonovanCS <jordan.t.donovancs@gmail.com>
2022-04-05 10:39:32 -0400

    added wandb logging

--- parallelHillClimber.py
+++ parallelHillClimber.py
@@ -2,6 +2,7 @@ import solution
 import constants as c
 import copy
 import os
+import wandb
 
 class PARALLEL_HILL_CLIMBER:
     def __init__(self):
@@ -14,11 +15,15 @@ class PARALLEL_HILL_CLIMBER:
         for i in range(c.populationSize):
             self.parents[i] = solution.SOLUTION(self.nextAvailableID)
             self.nextAvailableID += 1
+        self.currentGeneration=0
+        wandb.init('parallel-hill-climber')
+        wandb.config.version='deliverable 1'
     
     def Evolve(self):
         self.Evaluate(self.parents)
         
         for currentGeneration in range(c.numberOfGenerations):
+            self.currentGeneration = currentGeneration
             self.Evolve_For_One_Generation()
 
     def Evolve_For_One_Generation(self):
@@ -26,6 +31,7 @@ class PARALLEL_HILL_CLIMBER:
         self.Mutate()
         self.Evaluate(self.children)
         self.Print()
+        self.Record()
         self.Select()
 
     def Spawn(self):
@@ -56,6 +62,15 @@ class PARALLEL_HILL_CLIMBER:
         for k in self.parents:
             print('parent: {}, child: {}'.format(self.parents[k].fitness, self.children[k].fitness))
         print()
+
+    def Record(self):
+        best_key = 0
+        best_value = 0
+        for k in self.parents:
+            if self.parents[k].fitness < best_value:
+                best_value = self.parents[k].fitness
+                best_key = k
+        wandb.log({'gen': self.currentGeneration, 'best_fitness': best_value, 'num_hidden': self.parents[best_key].numHiddenNeurons})
 
     def Show_Best(self):
         best_key = 0
