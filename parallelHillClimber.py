be1f1ae5ec36e9d9dc9d5155522caa6201b00ed4
jdonovanCS <jordan.t.donovancs@gmail.com>
2022-04-05 11:26:30 -0400

    Revert "added logging to wandb"
    This reverts commit 9d84f538f80db8828a6e73ab27cca2dddd581dc7.

--- parallelHillClimber.py
+++ parallelHillClimber.py
@@ -2,28 +2,21 @@ import solution
 import constants as c
 import copy
 import os
-import wandb
 
 class PARALLEL_HILL_CLIMBER:
     def __init__(self):
         os.system('del \"fitness*.txt\"')
         os.system('del \"brain*.nndf\"')
-        os.system('del \"world*.sdf\"')
-        os.system('del \"body*.urdf\"')
         self.parents = {}
         self.nextAvailableID = 0
         for i in range(c.populationSize):
             self.parents[i] = solution.SOLUTION(self.nextAvailableID)
             self.nextAvailableID += 1
-        self.currentGeneration=0
-        wandb.init('parallel-hill-climber')
-        wandb.config.version='quadruped-before-fp'
-
+    
     def Evolve(self):
         self.Evaluate(self.parents)
-
+        
         for currentGeneration in range(c.numberOfGenerations):
-            self.currentGeneration = currentGeneration
             self.Evolve_For_One_Generation()
 
     def Evolve_For_One_Generation(self):
@@ -31,22 +24,36 @@ class PARALLEL_HILL_CLIMBER:
         self.Mutate()
         self.Evaluate(self.children)
         self.Print()
-        self.Record()
         self.Select()
 
     def Spawn(self):
-@@ -57,6 +63,15 @@ def Print(self):
+        self.children = {}
+        for k in self.parents:
+            self.children[k] = copy.deepcopy(self.parents[k])
+            self.children[k].Set_ID(self.nextAvailableID)
+            self.nextAvailableID += 1
+
+    def Mutate(self):
+        for k in self.children:
+           self.children[k].Mutate()
+
+    def Evaluate(self, solutions):
+        for k in solutions:
+            solutions[k].Start_Simulation('DIRECT')
+
+        for k in solutions:
+            solutions[k].Wait_For_Simulation_To_End()
+
+    def Select(self):
+        for k in self.parents:
+            if self.children[k].fitness < self.parents[k].fitness:
+                self.parents[k] = copy.deepcopy(self.children[k])
+
+    def Print(self):
+        print()
+        for k in self.parents:
             print('parent: {}, child: {}'.format(self.parents[k].fitness, self.children[k].fitness))
         print()
-
-    def Record(self):
-        best_key = 0
-        best_value = 0
-        for k in self.parents:
-            if self.parents[k].fitness < best_value:
-                best_value = self.parents[k].fitness
-                best_key = k
-        wandb.log({'gen': self.currentGeneration, 'best_fitness': best_value, 'num_hidden': self.parents[best_key].numHiddenNeurons})
 
     def Show_Best(self):
         best_key = 0
@@ -57,3 +64,4 @@ class PARALLEL_HILL_CLIMBER:
                 best_key = k
         self.parents[best_key].Start_Simulation('GUI')
         
+
