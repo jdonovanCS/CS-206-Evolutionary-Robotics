7c5c9061c0f2f75a79686ce33bc6cc94c6c66561
DESKTOP-ESA8F4M\Jordan <jordan.t.donovancs@gmail.com>
2022-04-06 15:05:49 -0400

    wandb logging

--- parallelHillClimber.py
+++ parallelHillClimber.py
@@ -18,6 +18,7 @@ class PARALLEL_HILL_CLIMBER:
         self.currentGeneration=0
         wandb.init('parallel-hill-climber')
         wandb.config.version='fp-base'
+        wandb.config.population=c.populationSize
 
     def Evolve(self):
         self.Evaluate(self.parents)
