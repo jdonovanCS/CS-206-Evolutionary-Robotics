3a9dbaad1aa6ea913664583cb782d73ccf36e8bb
DESKTOP-ESA8F4M\Jordan <jordan.t.donovancs@gmail.com>
2022-04-19 15:48:57 -0400

    AB testing

--- constants.py
+++ constants.py
@@ -19,8 +19,8 @@ TARGET_ANGLES = targetBeforeSin
 
 SLEEP_RATE = 1/60
 
-populationSize = 200
-numberOfGenerations = 800
+populationSize = 25
+numberOfGenerations = 50
 
 numSensorNeurons = 9
 numMotorNeurons = 8
